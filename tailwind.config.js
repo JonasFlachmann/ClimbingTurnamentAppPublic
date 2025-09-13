8240ac696ce039a917549e1f5603d65458cfaff5
JonasFlachmann <jonas.flachmann@web.de>
2025-09-13 21:40:22 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
-        primary: "#0f3d2e",
-        accent: "#34d399"
-      }
-    }
+        darkgreen: "#0f3d2e",
+        lightgreen: "#34d399",
+      },
+    },
   },
   plugins: [],
 };
