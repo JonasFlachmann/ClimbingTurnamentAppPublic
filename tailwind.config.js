dea93217793ed49ef2b9f8a728a7966511d5e12b
JonasFlachmann <jonas.flachmann@web.de>
2025-09-13 20:32:06 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0f3d2e",
+        accent: "#34d399"
+      }
+    }
+  },
+  plugins: [],
+};
