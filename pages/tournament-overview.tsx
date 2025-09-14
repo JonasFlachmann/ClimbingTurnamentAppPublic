ffacd7a9d7e038780776790cb27e73b50e0034d0
JonasFlachmann <jonas.flachmann@web.de>
2025-09-14 10:58:02 +0200

    Create tournament-overview.tsx

---
+++ pages/tournament-overview.tsx
@@ -0,0 +1,7 @@
+export default function TournamentOverview() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-darkgreen">
+      <h1 className="text-white text-3xl">Turnierübersicht (Dummy)</h1>
+    </div>
+  );
+}
