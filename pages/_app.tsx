98ad2c2ebdd2eb34c4f4487aeea2821c7d5554d6
JonasFlachmann <jonas.flachmann@web.de>
2025-09-19 09:39:45 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,8 @@
 import type { AppProps } from "next/app";
 import { ThemeProvider, CssBaseline, GlobalStyles } from "@mui/material";
 import theme from "../styles/theme";
+import Layout from "../components/Layout";
 
-/**
- * Globaler Wrapper für Pages Router.
- * Achtung: Ich füge hier bewusst KEINEN Header/Footer ein,
- * um nichts an deinen bestehenden Seiten zu überschreiben oder zu duplizieren.
- * Sobald du Header/Footer global möchtest, bauen wir das optional
- * als eigenes Layout ein und ersetzen die lokalen Varianten schrittweise.
- */
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
@@ -19,15 +13,14 @@ export default function MyApp({ Component, pageProps }: AppProps) {
           body: {
             height: "100%",
             backgroundColor: theme.palette.background.default,
-            backgroundImage: "url('/background-plants.jpg')",
-            backgroundSize: "cover",
-            backgroundPosition: "center",
             margin: 0,
           },
           "#__next": { minHeight: "100%" },
         }}
       />
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ThemeProvider>
   );
 }
